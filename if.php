876999865ad8a1cdee378500384ebc5206d75953
root <root@eeeyes.org>
2013-07-19 14:30:38 +0800

    add some files

---
+++ if.php
@@ -0,0 +1,8 @@
+<?php
+
+	$my_name = "someguy";
+	
+	if($my_name == "someguy")
+		echo "your name is some guy" . "<br/>";
+	echo "welcome to my home page"
+?>
---
+++ ifelse.php
@@ -0,0 +1,8 @@
+<?php
+
+	$number_three = 3;
+	if ( $number_three == 3)
+		echo "it is three";
+	else
+		echo "it is not three";
+?>
